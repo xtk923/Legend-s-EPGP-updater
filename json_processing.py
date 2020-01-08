d24a92d7e3a605e7522616c9198133e5876360e7
Xiong Tiankai <xiongtk@gmail.com>
2020-01-08 21:02:34 +0800

    Guild rank with numerical sequence

--- json_processing.py
+++ json_processing.py
@@ -2,9 +2,23 @@
 import sys
 import pandas as pd
 
+guild_rank = {
+    "会长" : 1,
+    "副会长" : 2, 
+    "官员" : 3, 
+    "职业队长": 4,
+    "1团（EPGP）": 5,
+    "2团（EPGP）": 6,
+    "大红大紫团（G）": 7,
+    "会员": 8,
+    "见习": 9
+}
+
+
+
 def main():
     filename = sys.argv[1]
-    file = open(filename)
+    file = open(filename, "r", encoding="utf-8")
     raw = file.readline()
     file.close()
 
@@ -14,7 +28,7 @@ def main():
     content = raw.split(":")[1][0:-len(trail)]
 
 
-    cols = ['Character', 'Class', 'Rank', 'EP', 'GP', "PR"]
+    cols = ['角色', '职业', '会阶', 'EP', 'GP', "PR"]
     df = pd.read_json(content, encoding="gb2312")
     df.columns = cols
 
@@ -22,11 +36,16 @@ def main():
     df = df[df['EP'] != 0]
 
     # sort by rank and class
-    df = df.sort_values(by=['Rank', 'Class'])
-    tableLine = pd.DataFrame([['---' for x in range(len(df.columns.values))]],
-                            columns = df.columns.values)
+    df['guild rank'] = df['会阶'].map(guild_rank)
+
+    df = df.sort_values(by=['guild rank', '职业'])
+    del df['guild rank']
+    tableLine = pd.DataFrame(
+        [['---' for x in range(len(df.columns.values))]],
+        columns = df.columns.values)
 
     df = pd.concat([tableLine, df])
+
     print(df.head())
     df.to_csv('./result.md', encoding='utf-8', index=False, sep="|")
 
