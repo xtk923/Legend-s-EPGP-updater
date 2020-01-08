137cbbc3b9c9207892893f0b1cee24e50bec3d91
Xiong Tiankai <rmixt@nus.edu.sg>
2020-01-08 17:01:48 +0800

    first commit'

---
+++ json_processing.py
@@ -0,0 +1,29 @@
+#-*-coding=utf-8-*-
+import sys
+import pandas as pd
+
+file = open('./epgp.txt')
+raw = file.readline()
+file.close()
+
+# This JSON file is not properly formated.
+# We crop its middle part as the pure content.
+trail = ",\"timestamp\""
+content = raw.split(":")[1][0:-len(trail)]
+
+
+cols = ['Character', 'Class', 'Rank', 'EP', 'GP', "PR"]
+df = pd.read_json(content, encoding="gb2312")
+df.columns = cols
+
+# remove those with zero EP
+df = df[df['EP'] != 0]
+
+# sort by rank and class
+df = df.sort_values(by=['Rank', 'Class'])
+tableLine = pd.DataFrame([['---' for x in range(len(df.columns.values))]],
+                         columns = df.columns.values)
+
+df = pd.concat([tableLine, df])
+print(df.head())
+df.to_csv('./result.md', encoding='utf-8', index=False, sep="|")
