ff6cb7955f095cf37995d900ef3e31eafb2c2726
Xiong Tiankai <rmixt@nus.edu.sg>
2020-01-08 17:07:41 +0800

    make it a CLT

--- json_processing.py
+++ json_processing.py
@@ -2,28 +2,33 @@
 import sys
 import pandas as pd
 
-file = open('./epgp.txt')
-raw = file.readline()
-file.close()
+def main():
+    filename = sys.argv[1]
+    file = open(filename)
+    raw = file.readline()
+    file.close()
 
-# This JSON file is not properly formated.
-# We crop its middle part as the pure content.
-trail = ",\"timestamp\""
-content = raw.split(":")[1][0:-len(trail)]
+    # This JSON file is not properly formated.
+    # We crop its middle part as the pure content.
+    trail = ",\"timestamp\""
+    content = raw.split(":")[1][0:-len(trail)]
 
 
-cols = ['Character', 'Class', 'Rank', 'EP', 'GP', "PR"]
-df = pd.read_json(content, encoding="gb2312")
-df.columns = cols
+    cols = ['Character', 'Class', 'Rank', 'EP', 'GP', "PR"]
+    df = pd.read_json(content, encoding="gb2312")
+    df.columns = cols
 
-# remove those with zero EP
-df = df[df['EP'] != 0]
+    # remove those with zero EP
+    df = df[df['EP'] != 0]
 
-# sort by rank and class
-df = df.sort_values(by=['Rank', 'Class'])
-tableLine = pd.DataFrame([['---' for x in range(len(df.columns.values))]],
-                         columns = df.columns.values)
+    # sort by rank and class
+    df = df.sort_values(by=['Rank', 'Class'])
+    tableLine = pd.DataFrame([['---' for x in range(len(df.columns.values))]],
+                            columns = df.columns.values)
 
-df = pd.concat([tableLine, df])
-print(df.head())
-df.to_csv('./result.md', encoding='utf-8', index=False, sep="|")
+    df = pd.concat([tableLine, df])
+    print(df.head())
+    df.to_csv('./result.md', encoding='utf-8', index=False, sep="|")
+
+if __name__ == "__main__":
+    main()
